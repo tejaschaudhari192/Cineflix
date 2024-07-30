c2f2b9ab4e231f82010c48936aecbed5e2a8d5c1
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-07-30 08:37:08 +0530

    Hygiene Practice: Unsubscribed to OnAuthchange Callback

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
     }
 
     useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 // User is signed in, see docs for a list of available properties
                 // https://firebase.google.com/docs/reference/js/auth.user
@@ -53,6 +53,7 @@ const Header = () => {
             }
         });
 
+        return () => unsubscribe()
     }, [])
 
     return (
