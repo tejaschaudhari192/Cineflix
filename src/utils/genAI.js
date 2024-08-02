b635634c7a871f353c8bfa396c872d5009980967
Tejas Chaudhari <jaybalaji192@gmail.com>
2024-08-02 22:36:01 +0530

    won't working : safety settings added

--- src/utils/genAI.js
+++ src/utils/genAI.js
@@ -1,9 +1,33 @@
-import {GoogleGenerativeAI} from "@google/generative-ai";
+import {GoogleGenerativeAI, HarmBlockThreshold, HarmCategory} from "@google/generative-ai";
 import {GEMINI_KEY} from "./constants.js";
 
 const genAI = new GoogleGenerativeAI(
     GEMINI_KEY
 )
+const safetySettings = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+        threshold: HarmBlockThreshold.BLOCK_NONE
+    }
+];
+
+export const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"}, safetySettings);
 
 
 export default genAI;
